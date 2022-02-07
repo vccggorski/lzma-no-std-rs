3fb86b7bf45ef5a900c83f2827688c922ed7dcc1
Gabriel Górski <gabriel.gorski@volvocars.com>
2022-02-07 13:48:13 +0100

    Change hardcoded dictionary size in dumbencoder to 4096 bytes

--- src/encode/dumbencoder.rs
+++ src/encode/dumbencoder.rs
@@ -23,7 +23,7 @@ where
     W: io::Write,
 {
     pub fn from_stream(stream: &'a mut W, options: &Options) -> io::Result<Self> {
-        let dict_size = 0x0080_0000;
+        let dict_size = 4096;
 
         // Properties
         let props = (LC + 9 * (LP + 5 * PB)) as u8;
